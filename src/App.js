23b749aefd68a7d18f803aaa0fb6465edcb2aa38
David Rose <doppler@gmail.com>
2019-01-30 13:55:21 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ColorWheel from "./components/ColorWheel";
+import "./App.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <ColorWheel numberOfRadials={55} />
+      <ColorWheel numberOfRadials={55} reverse />
+      <About />
+    </div>
+  );
+};
 
 export default App;
+
+const About = () => (
+  <div className="About">
+    <article>
+      <header>
+        <div className="title">David Rose</div>
+        <div className="email">
+          <a href="mailto:doppler@gmail.com">doppler@gmail.com</a>
+        </div>
+      </header>
+      <section>
+        <header>2014 - present:</header>
+        <p>
+          Full-time{" "}
+          <a
+            href="https://uspa.org"
+            title="United States Parachute Association"
+          >
+            USPA
+          </a>{" "}
+          skydiving instructor, videographer and{" "}
+          <abbr title="Safety and Training Advisor">S&amp;TA</abbr> at{" "}
+          <a
+            href="https://atlanta.skydivespaceland.com"
+            title="Skydive Spaceland Atlanta"
+          >
+            Skydive Spaceland Atlanta
+          </a>
+          , holding Tandem, AFF, Static Line and IAD instructional ratings.
+        </p>
+      </section>
+      <section>
+        <header>1999 - 2014:</header>
+        <p>
+          Lead web application developer at{" "}
+          <a
+            href="https://sxsw.com"
+            title="South by Southwest Conferences and Festivals (SXSW)"
+          >
+            South by Southwest (SXSW)
+          </a>
+        </p>
+      </section>
+    </article>
+  </div>
+);
---
+++ src/components/ColorWheel.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./ColorWheel.scss";
+
+export const ColorWheel = ({ numberOfRadials, reverse }) => {
+  return (
+    <div className={`ColorWheel ${reverse ? "reverse" : null}`}>
+      {Array.from(Array(numberOfRadials)).map((_, i) => (
+        <Radial key={i} rotation={(i * 360) / numberOfRadials} />
+      ))}
+    </div>
+  );
+};
+export const Radial = ({ rotation }) => (
+  <div
+    className="Radial"
+    style={{
+      transform: `rotate(${rotation}deg)`,
+      backgroundColor: `hsl(${rotation}, 100%, 50%)`
+    }}
+  />
+);
+export const RadialCountSlider = ({ numberOfRadials, handleRangeChange }) => (
+  <input
+    id="RadialCountSlider"
+    type="range"
+    value={numberOfRadials}
+    min={0}
+    max={360}
+    onChange={handleRangeChange}
+  />
+);
+
+export default ColorWheel;
