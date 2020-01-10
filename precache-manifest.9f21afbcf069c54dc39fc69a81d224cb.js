2c6f0bcfaa0444a922dc67ce39a4c547bdd374d5
David Rose <doppler@gmail.com>
2020-01-09 21:36:35 -0500

    Updates

---
+++ precache-manifest.9f21afbcf069c54dc39fc69a81d224cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e3c341e6179e5ea7e86",
+    "url": "/static/css/main.133ad8bb.chunk.css"
+  },
+  {
+    "revision": "4e3c341e6179e5ea7e86",
+    "url": "/static/js/main.4e3c341e.chunk.js"
+  },
+  {
+    "revision": "7b70c3a40d5c91ed34f5",
+    "url": "/static/js/1.7b70c3a4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54e9057aa79f5052b26bb6703aeab8b1",
+    "url": "/index.html"
+  }
+];
